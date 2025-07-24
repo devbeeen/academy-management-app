1d0c2d16f963c5bdf578bd775142d7cadd25a2f1
tohjbin2 <tohjbin2@gmail.com>
2025-07-25 05:39:28 +0900

    feat: supabase 설치

--- src/components/Main/Contents.tsx
+++ src/components/Main/Contents.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../../supabaseClient';
 
 export const Contents = () => {
-  return <div>Main Page Contents</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchTestData = async () => {
+      const { data, error } = await supabase.from('test_table').select('*');
+      console.log('data1:', data);
+      console.log('error:', error);
+
+      if (error) {
+        console.error('Supabase 연동 실패:', error.message);
+      } else {
+        setData(data);
+        console.log('data2:', data);
+      }
+    };
+
+    fetchTestData();
+  }, []);
+
+  return (
+    <div>
+      Main Page Contents
+      {data.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </div>
+  );
 };
