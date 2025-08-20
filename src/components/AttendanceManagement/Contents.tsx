ea7bbcd74f5d2dd6c983132a5f89d2707e1b9211
tohjbin2 <tohjbin2@gmail.com>
2025-08-20 20:38:50 +0900

    modify: memo, useMemo로 업데이트 데이터 관리 및 스타일 추가

--- src/components/AttendanceManagement/Contents.tsx
+++ src/components/AttendanceManagement/Contents.tsx
@@ -1,13 +1,12 @@
 // 수강생 등록 컴포넌트
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { supabase } from '../../supabaseClient';
-
 import useUserStore from '../../store/userStore';
 import { useShallow } from 'zustand/react/shallow';
 
 import { Contents as DailyAttendance } from '../DailyAttendance/Contents';
-import { Modal } from '../../lib/UI/Modal/Modal';
+import { Button } from '../../lib/UI/Button';
 import { toKST } from '../../lib/utils/toKST';
 
 // import { DayPicker } from 'react-day-picker'; // react-day-picker 라이브러리
@@ -16,10 +15,11 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'; // MUI 데이터 피커
 import dayjs, { Dayjs } from 'dayjs';
 
-export const Contents = () => {
-  const [memberName, setMemberName] = useState(); // 수강생 이름(등록을 위한)
-  const [isAddMemberOpen, setIsAddMemberOpen] = useState(false); // 수강생 등록 모달
+import { TextField } from '@mui/material';
 
+import styled from 'styled-components';
+
+export const Contents = () => {
   // DatePicker 값 저장용 state
   const [dateValue, setDateValue] = useState<Dayjs | null>(dayjs());
 
@@ -27,45 +27,11 @@ export const Contents = () => {
   const targetMonth = (dateValue.month() + 1).toString().padStart(2, '0');
   const targetDate = dateValue.date().toString().padStart(2, '0');
 
-  console.log('공통-targetYear: ', targetYear);
-  console.log('공통-targetYear: ', targetMonth);
-  console.log('공통-targetYear: ', targetDate);
-
-  // 👇 userStore(로그인 유저 정보) 불러오기 -----
-  // const pullData = useUserStore(
-  //   useShallow(state => ({
-  //     name: state.id,
-  //   })),
-  // );
+  // [TAG] fetchUserStore: userStore(로그인한 유저 정보 store) 불러오기
   const fetchUserStore = useUserStore(useShallow(state => state));
-  console.log('fetchUserStore', fetchUserStore);
-  // ☝ userStore(로그인 유저 정보) 불러오기 -----
-
   const currentCompanyID = fetchUserStore.companyID;
 
-  const memberNameValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMemberName(e.target.value);
-  };
-
-  // onAddMember: 수강생 등록 함수
-  const onAddMember = async () => {
-    const { data, error } = await supabase
-      .from('members') // 테이블명
-      .insert([
-        {
-          name: memberName,
-          company_id: currentCompanyID,
-        },
-      ]);
-
-    if (error) {
-      console.error('수강생 등록 실패:', error.message);
-    } else {
-      console.log('수강생 등록 성공:', data);
-    }
-  };
-
-  // onFetchMemberList: members 테이블 불러오기 (수강생 불러오기)
+  // [TAG] onFetchMemberList(): members 테이블 불러오기(수강생 불러오기)
   /**
    * DB에 조인 요청 -> DB는 내부적으로 JOIN을 수행 -> 결과(중첩된 데이터)를 JSON 형태로 프론트에 돌려줌
    * 기본적으로 조인은 DB에서 이루어지는 쿼리 작업. 아래 코드는 프론트상에서 조인을 요청한 것.
@@ -91,7 +57,14 @@ export const Contents = () => {
     }
   };
 
-  // onProcessedData: 데이터 가공 -----
+  /**
+   * maxMinute: 하루를 10분 단위로 쪼갰을 때 수치.
+   * - 총 144개(하루 24시 x 시간당 6칸).
+   * - maxMinute 값은 143(배열 0~143).
+   */
+  const maxMinute = 143;
+  const processedData = [];
+
   /*
   // 실시간 데이터 감지시에는 활성
   const currentDate = new Date();
@@ -102,27 +75,17 @@ export const Contents = () => {
   );
   */
 
-  /**
-   * [MEMO] maxMinute: 하루를 10분 단위로 쪼갰을 때 수치.
-   * - 총 144개(하루 24시 x 시간당 6칸).
-   * - maxMinute 값은 143(배열 0~143).
-   */
-  const maxMinute = 143;
-  const processedData = [];
-
-  // 데이터 가공
+  // [TAG] onProcessedData(): 데이터 가공
   function onProcessedData() {
     if (memberList) {
-      console.log('💬memberList:', memberList);
+      // console.log('AttendanceManagement-memberList:', memberList);
 
       memberList.forEach(memberItem => {
         const oneDayData = [];
 
         if (memberItem.attendance_date.length > 0) {
-          console.log('🦅🦅🦅memberItem:', memberItem);
-
           /**
-           * [MEMO] progressTime: 하루 총 시간 (하루 10분 단위 x 144개(maxMinute))
+           * progressTime: 하루 총 시간 (하루 10분 단위 x 144개(maxMinute))
            * 1시간당 10분 단위로 나타내기로 결정 -> 하루(24시) x 시간당 6칸 = 총 144개
            */
           for (
@@ -131,14 +94,10 @@ export const Contents = () => {
             progressTime++
           ) {
             memberItem.attendance_date.forEach(attendanceItem => {
-              // console.log('attendanceItem: ', attendanceItem);
-
               let perTimeData = attendanceItem.attendance_status.map(
                 statusItem => {
-                  // console.log('statusItem: ', statusItem);
-
                   /**
-                   * [MEMO] checkInTimeHour, checkInTimeMinute:
+                   * checkInTimeHour, checkInTimeMinute:
                    * 시작 시간(시, 분 단위)만 추출 + undefined인 경우 빈 문자열 반환(분기 처리).
                    */
                   // 2025-07-28T09:30:00+00:00
@@ -157,7 +116,7 @@ export const Contents = () => {
 
                   const processedCheckInTime = Math.ceil(
                     /**
-                     * [MEMO] (Number(checkInTimeHour) * 60 + Number(checkInTimeMinute)) / 10):
+                     * (Number(checkInTimeHour) * 60 + Number(checkInTimeMinute)) / 10):
                      * 1-1. Number(checkInTimeHour) = 출근(시간 단위)
                      * 1-2. Number(checkInTimeHour) * 60: 시간 단위를 분 단위로 환산. 시간에 60을 곱한다.
                      * 1-3. Number(checkInTimeMinute) = 출근(분 단위)
@@ -171,7 +130,6 @@ export const Contents = () => {
 
                   let processedCheckOutTime = 0;
                   if (statusItem.check_out_time !== '') {
-                    // processedCheckOutTime = Math.floor( // 이전
                     processedCheckOutTime = Math.ceil(
                       (Number(checkOutTimeHour) * 60 +
                         Number(checkOutTimeMinute)) /
@@ -185,8 +143,8 @@ export const Contents = () => {
                     processedCheckOutTime = maxMinute;
                   }
 
-                  // [TAG] 현재 시간 체크를 위한 조건 -----start
-                  // 실시간으로 찍히는 경우 (퇴근 기록 없음)
+                  // [TAG] 현재 시간 체크를 위한 조건
+                  // 실시간으로 찍히는 경우(수업 종료 기록 없음)
                   /*
                   if (
                     statusItem.attendance_code === 'M100' &&
@@ -197,15 +155,14 @@ export const Contents = () => {
                       priority: 1,
                       time: progressTime,
                       statusName: '',
-                      dataName: '퇴근 기록 없음',
+                      dataName: '데이터 없음',
                     });
                   }
                   */
-                  // [TAG] 현재 시간 체크를 위한 조건 -----end
 
                   if (
                     /**
-                     * [MEMO] 조건: 체크인-체크아웃 날짜 불일치
+                     * [TAG] 조건: 체크인-체크아웃 날짜 불일치
                      * Number(statusItem.check_in_time.slice(8, 10)) !== Number(statusItem.check_out_time.slice(8, 10))
                      */
                     statusItem.attendance_code === 'M100' &&
@@ -214,49 +171,39 @@ export const Contents = () => {
                     processedCheckInTime <= progressTime
                   ) {
                     return (perTimeData = {
-                      // priority: 98, // 원본
                       priority: 100,
                       time: progressTime,
-                      statusName: '불일치statusName-등원',
+                      statusName: '불일치(statusName)-등원',
                       dataName: '불일치-등원',
-                      statusColor: 'olive', // 🍔
-                      hasData: true, // 🍔
+                      statusColor: '#cdcdcd', // 추가
+                      hasData: true, // 추가
                     });
                   } else if (
-                    // [MEMO] 조건: 체크인-체크아웃 날짜 일치
+                    // [TAG] 조건: 체크인-체크아웃 날짜 일치
                     statusItem.attendance_code === 'M100' &&
                     processedCheckInTime <= progressTime &&
                     processedCheckOutTime >= progressTime
                   ) {
-                    // console.log('일치---등원');
-
                     return (perTimeData = {
-                      // priority: 98, // 원본
                       priority: 100,
                       time: progressTime,
-                      statusName: '일치statusName-등원',
+                      statusName: '일치(statusName)-등원',
                       dataName: '일치-등원',
-                      statusColor: 'olive', // 🍔
-                      hasData: true, // 🍔
+                      statusColor: '#cdcdcd', // 추가
+                      hasData: true, // 추가
                     });
                   } else {
-                    // 뉴)
                     return (perTimeData = {
-                      // priority: 99, // 원본
                       priority: 101,
                       time: progressTime,
                       statusName: '',
-                      // dataName: '데이터없음',
-                      hasData: false, // 🍔
+                      hasData: false, // 추가
                     });
                   }
                 },
               );
 
-              /**
-               * [MEMO] priorityData:
-               * 만약 동일한 시간대에 데이터가 들어온다면(데이터 중복시), sort로 정렬
-               */
+              // [TAG] priorityData: 만약 동일한 시간대에 데이터가 들어온다면(데이터 중복시), sort로 정렬
               const priorityData = perTimeData.sort(
                 (a: { priority: number }, b: { priority: number }) => {
                   return a.priority - b.priority;
@@ -266,10 +213,7 @@ export const Contents = () => {
             });
           }
         } else {
-          /**
-           * [MEMO] else 부분:
-           * - 0에서부터 23시까지 데이터가 아예 없는 경우. 하루 내역이 아예 없는 경우.
-           */
+          // [TAG] else: 0에서부터 23시까지 데이터가 아예 없는 경우. 하루 내역이 아예 없는 경우.
           for (let processTime = 0; processTime <= maxMinute; processTime++) {
             const result = {
               time: processTime,
@@ -283,16 +227,17 @@ export const Contents = () => {
         const result = {
           id: memberItem.id,
           /**
-           * attendance_date[0]가 있으면 -> attendanceDateID에 memberItem.attendance_date[0].id를 추가
+           * attendance_date[0]가
+           * 있으면 -> attendanceDateID 키 값으로 memberItem.attendance_date[0].id를 추가
            * 없으면 -> 아무 것도 추가하지 않음
            */
           ...(memberItem.attendance_date[0] && {
             attendanceDateId: memberItem.attendance_date[0].id,
           }),
-          name: memberItem.name,
           ...(memberItem.attendance_date[0] && {
             date: memberItem.attendance_date[0].date,
-          }), // 📢 새로운 추가: 날짜
+          }),
+          name: memberItem.name,
           oneDayData: oneDayData,
         };
         processedData.push(result);
@@ -301,76 +246,86 @@ export const Contents = () => {
   }
   onProcessedData();
 
-  // console.log('processedData: ', processedData);
-
   return (
     <div>
-      출결 확인
       <div
         style={{
           display: 'flex',
+          justifyContent: 'start',
+          alignItems: 'center',
         }}
       >
-        <div style={{ position: 'relative', display: 'inline-block' }}>
-          {/* 달력 */}
+        <div>
+          {/* 데이트 피커 */}
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
               value={dateValue}
               onChange={newValue => setDateValue(newValue)}
+              enableAccessibleFieldDOMStructure={false}
+              slots={{ textField: TextField }} // 반드시 타입만
+              slotProps={{
+                textField: {
+                  sx: {
+                    width: '12rem',
+                    backgroundColor: '#f3f3f3',
+                    '& .MuiOutlinedInput-root': {
+                      height: '45px',
+                      '& input': {
+                        padding: '0.4rem',
+                      },
+                    },
+                    // 인풋 글씨 (년/월/일)
+                    '& .MuiInputBase-input': {
+                      fontSize: '0.8rem',
+                      fontWeight: 500,
+                      color: 'gray',
+                    },
+                    '& .MuiOutlinedInput-notchedOutline': {
+                      borderRadius: '0.4rem',
+                      borderColor: '#B2B2B2',
+                    },
+                  },
+                },
+                day: {
+                  sx: {
+                    '&.MuiPickersDay-dayToday': {
+                      //selected 상태이면서 today인 경우
+                      borderColor: '#fb460d',
+                      backgroundColor: '#fb460d',
+                      color: 'white',
+                    },
+                    '&.MuiPickersDay-today': {
+                      // 오늘 날짜
+                      borderColor: '#fb460d',
+                      backgroundColor: '#fb460d',
+                      color: 'white',
+                    },
+                    '&.Mui-selected': {
+                      // 선택된 날짜
+                      borderColor: '#fc6714',
+                      backgroundColor: '#fc6714',
+                      color: 'white',
+                    },
+                  },
+                },
+                popper: {
+                  // 달력 팝업
+                  sx: {
+                    '& .MuiPaper-root': {
+                      backgroundColor: 'white',
+                      borderRadius: '0.4rem',
+                      padding: '8px',
+                    },
+                  },
+                },
+              }}
             />
           </LocalizationProvider>
         </div>
-        <div>
-          {isAddMemberOpen && (
-            <Modal onClose={() => setIsAddMemberOpen(false)} width="500px">
-              <div style={{ width: '400px', border: 'solid 1px black' }}>
-                <div>수강생 등록</div>
-                <div>
-                  <label>이름</label>
-                  <input
-                    placeholder="이름을 입력해주세요"
-                    style={{ height: '60px', border: 'solid 1px black' }}
-                    onChange={memberNameValue}
-                  />
-                </div>
-                <button
-                  label="수강생 등록"
-                  style={{
-                    width: '80px',
-                    height: '60px',
-                    border: 'solid 1px black',
-                  }}
-                  onClick={() => onAddMember()}
-                >
-                  등록
-                </button>
-                <button
-                  style={{
-                    width: '80px',
-                    height: '60px',
-                    border: 'solid 1px black',
-                  }}
-                  onClick={() => setIsAddMemberOpen(false)}
-                >
-                  닫기
-                </button>
-              </div>
-            </Modal>
-          )}
-          <button
-            style={{ width: '80px', height: '60px', border: 'solid 1px black' }}
-            onClick={() => setIsAddMemberOpen(true)}
-          >
-            수강생 등록(모달)
-          </button>
-        </div>
-        <div>
-          <button
-            style={{ width: '80px', height: '60px', border: 'solid 1px black' }}
-            onClick={() => onFetchMemberList()}
-          >
-            수강생 불러오기
-          </button>
+        <div style={{ marginLeft: '0.4rem' }}>
+          <Button handleClick={() => onFetchMemberList()}>
+            데이터 불러오기
+          </Button>
         </div>
       </div>
       <DailyAttendance
@@ -378,6 +333,7 @@ export const Contents = () => {
         year={targetYear}
         month={targetMonth}
         date={targetDate}
+        onRefresh={onFetchMemberList} // 추가
       />
     </div>
   );
