8e7e31044a126e4784c1ad93553831586ebe22ae
tohjbin2 <tohjbin2@gmail.com>
2025-08-29 00:14:04 +0900

    modift: 카테고리 스토리지 데이터 삭제 추가

--- src/lib/utils/onSignOut.ts
+++ src/lib/utils/onSignOut.ts
@@ -1,8 +1,11 @@
 import { supabase } from '../../supabaseClient';
 import useUserStore from '../../store/userStore';
+import { useCategoryStore } from '../../store/categoryStore';
 
 export async function onSignOut() {
   useUserStore.persist.clearStorage();
+  useCategoryStore.persist.clearStorage();
+
   const { error } = await supabase.auth.signOut();
 
   if (error) {
