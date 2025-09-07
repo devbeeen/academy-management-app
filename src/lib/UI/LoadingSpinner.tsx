1c621bd563692ce2925ebbcb26223e7380360642
tohjbin2 <tohjbin2@gmail.com>
2025-09-07 19:44:03 +0900

    feat: 로딩 스피느 스토어, 컴포넌트 추가

---
+++ src/lib/UI/LoadingSpinner.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { useUIStore } from '../../store/uiStore';
+
+interface LoadingSpinnerProps {
+  isLoading: boolean;
+}
+
+export const LoadingSpinner = ({ isLoading }: LoadingSpinnerProps) => {
+  if (!isLoading) return null;
+
+  // const isLoading = useUIStore(state => state.isLoading); // 로딩 스피너
+  // const handleLoading = useUIStore(state => state.handleLoading); // 로딩 스피너
+
+  // if (isLoading) {
+  //   return (
+  //     <>
+  //      <LoadingWrap>
+  //         <LoadingGIF src="/data/loading-200x200.gif" />
+  //       </LoadingWrap>
+  //     </>
+  //   );
+  // }
+
+  return (
+    <>
+      <LoadingWrap>
+        <LoadingGIF src="/data/loading-200x200.gif" />
+      </LoadingWrap>
+    </>
+  );
+};
+
+const LoadingWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 80px;
+  min-height: 300px; // 자리 확보
+`;
+
+const LoadingGIF = styled.img`
+  ${({ theme }) => theme.loadingGIF};
+  width: 6rem;
+  height: 6rem;
+`;
